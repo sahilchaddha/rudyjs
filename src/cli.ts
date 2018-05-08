cebab90ada8acdc0cfb946189738957bd626a588
Sahil Chaddha <sahilchaddha@gmail.com>
2018-05-08 12:00:51 +0800

    Updated CLI

--- src/cli.ts
+++ src/cli.ts
@@ -10,11 +10,23 @@ import * as program from "commander"
 import Rudy from "./main"
 import ASCIIBanner from "./utils/ascii"
 
-// Shows ASCII banner
-ASCIIBanner.showInfo()
-
 // Parse Arguments
 program
 .version("1.0.0", "-v, --version")
-.option("-h, --host", "Host to attack")
+.description("Sends slow RUDY Requests to target.")
+.option("-t, --target <string>", "Target URL to attack")
+.option("-c, --maxConnections <number>", "The number of connections to run simultaneously (Default: 50)")
+.option("-a, --userAgent <string>", "Provide a Static User Agent. (Default: Random)")
+.option("-d, --delay <number>", "Wait <seconds> before sending each byte. (Default: Random)")
+.option("-v, --verbose", "Enable Verbose Logs (Default: false)")
 .parse(process.argv)
+
+// Shows ASCII banner
+ASCIIBanner.showInfo()
+.then(() => {
+    // Show Help if Target is not specified
+    if (program.target == null) {
+        program.help()
+        process.exit(1)
+    }
+})
--- src/utils/ascii.ts
+++ src/utils/ascii.ts
@@ -11,15 +11,19 @@ import * as Constants from "./constants"
 /* tslint:disable no-console */
 
 class ASCIIBanner {
-    public static showInfo() {
-        figlet(Constants.projectName, (err, data) => {
-            if (err) {
-                return
-            }
-            console.log(data)
-            console.log("Author : " + Constants.authorName)
-            console.log("Website: " + Constants.authorWebsite)
-            console.log("Github : " + Constants.authorGithubLink)
+    public static showInfo(): Promise<boolean> {
+        return new Promise<boolean>((resolve, reject) => {
+            figlet(Constants.projectName, (err, data) => {
+                // No Need to Reject, Its fine if Banner is not shown
+                if (err) {
+                    return resolve(false)
+                }
+                console.log(data)
+                console.log("Author : " + Constants.authorName)
+                console.log("Website: " + Constants.authorWebsite)
+                console.log("Github : " + Constants.authorGithubLink)
+                return resolve(true)
+            })
         })
     }
 }
